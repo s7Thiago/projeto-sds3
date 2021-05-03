8af6481d49b337f6345f849515846a4cedc76e0e
Thiago <thyagosousasilva@gmail.com>
2021-05-03 16:17:09 -0300

    Project created

---
+++ backend/src/test/java/com/thiagosilva/tsvendas/TsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagosilva.tsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
