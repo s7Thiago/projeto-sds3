616096221b64ef9ae04f24229a8fca3746c763cc
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-05 18:55:26 -0300

    Dia 2 - Security config class added - Login screen do not is shown

---
+++ backend/src/main/java/com/thiagosilva/tsvendas/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.thiagosilva.tsvendas.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration // Usamos quando a classe é criada com o propósito de ser uma classe de configuração
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		Libera o aplicativo de gerenciamento do banco de dados h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+//		Define que a aplicação não vai manter estado (será stateless)
+//		se comportando de acordo com o que preconiza o padrão REST
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+//	Define as configurações de cors da aplicação, liberando acesso aos recursos para aplicações 
+//	externas que não estejam acessando a mesma do mesmo domínio corrente.
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
