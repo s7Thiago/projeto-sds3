ea58a45c0a8e06cc44dac11c449cf2cfda737b48
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-10 20:52:48 -0300

    Env var created

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = 'http://localhost:8080';
+// Aqui definimos que a BASE_URL da aplicação tem a intensão de pegar a variável de ambiente
+//  definida no ambiente de deploy, caso ela nào exista, ele usa a url do backend local
+// (o prefixo REACT_APP_*** é o padrão esperado pelo ambiente (netlify)), ou seja, quando a
+// aplicação estiver de fato no ambiente em nuvem, a url REACT_APP_BACKEND_URL sempre será usada
+// pois a mesma estará definida, já em ambiente local, será usado a url do host local
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
