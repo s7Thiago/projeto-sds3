97c50916863605e95bf476e532947e50a7a64324
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-10 19:31:50 -0300

    Bar chart integration

--- frontend/src/components/BarChart/barchart.tsx
+++ frontend/src/components/BarChart/barchart.tsx
@@ -1,6 +1,62 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
+import { SaleSuccess } from 'types/sale'
+import { round } from 'utils/format'
+import { BASE_URL } from 'utils/requests'
+
+type SeriesData = {
+    name: string;
+    data: number[],
+}
+
+type ChartData = {
+    labels: {
+        categories: string[]
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
+
+    // useState para guardar um ChartData
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: '',
+                data: []
+            }
+        ]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+
+            // Convertendo a resposta para uma lista de objetos do tipo SaleSum
+            const data = response.data as SaleSuccess[];
+
+            // Mapeando os valores de sellerName e de sum para listas
+            // é necessário para que seja possível usar no gráfico devido
+            // ao formato de entrada que ele requer
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100 * x.deals / x.visited, 1));
+
+            setChartData({
+                labels: {
+                    categories: myLabels
+                },
+                series: [
+                    {
+                        name: '% Success',
+                        data: mySeries
+                    }
+                ]
+            });
+        });
+    }, []);
 
     const options = {
         plotOptions: {
@@ -10,22 +66,10 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-
     return (
         <Chart
-            options={{ ...options, xaxis: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, xaxis: chartData.labels }}
+            series={chartData.series}
             type="bar"
             height="240"
 
